e70fff11d24a0c4044e34e0ef5dd0d4a335ed015
Mario Krajacic <krajacic.mario@gmail.com>
2020-08-08 19:38:58 +0200

    Adding TODO

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    return render_template()
+
+
+@app.route('/<usr>')
+def user(usr):
+    return render_template()
+
+# TODO: Nastavka je ovdje: https://youtu.be/9MHYHgh4jYc?t=244
+
 if __name__ == '__main__':
     app.run(debug=True)
